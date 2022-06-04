8a4b97bbaa45ed7cf369ddf3bb0fbf557a18601a
FishingDonut <protronsdimi@gmail.com>
2022-06-04 14:14:15 -0300

    1 commint

---
+++ delbeta.py
@@ -0,0 +1,23 @@
+import os
+
+melon = input("■■■■🍖 :") + '/'
+
+os.chdir(melon)
+
+homer = os.listdir()
+
+mw = []
+mwd = []
+
+for home in homer:
+    mw.append(home)
+
+
+for egg in mw:
+    if os.path.isdir(egg):
+        mwd.append(egg)
+
+for vinte in mwd:
+    os.rmdir(vinte)
+
+print(os.listdir())
